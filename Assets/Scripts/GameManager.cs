d6c6c96fd7354cf6de37a39f03387bc2abb3c6ca
pheeb <1033478@student.sae.edu.au>
2024-06-11 11:16:39 +1000

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    bool GameRunning = true;
+    bool GameRunning = true; // Variable to exit game loop when finished
 
+    // Player variables
     float player_healthPoints = 20.0f;
+    
     float player_attackPoints = 2.0f;
     float player_attackModifier = 1.0f;
-    float player_xp = 0.0f;
+
     int player_level = 1; 
+
+    float player_xp = 0.0f;
     float player_xpModifier = 1.0f;
     float player_xpThreshold = 5.0f;
 
+    // Enemy variables
     float enemy_healthPoints = 15.0f;
     float enemy_attackPoints = 2.0f;
     int enemy_level = 1; 
@@ -34,25 +39,26 @@ public class GameManager : MonoBehaviour
              * used if(GameRunning == true) in place of While gameRunning = True Do as it will do the same thing
              */
 
-            if (player_healthPoints >= 1)
+            if (player_healthPoints >= 1) // If HP is still above or equal to 1, continue loop.
             {
-                if (player_level >= 5)
+                if (player_level >= 5) // If player level is above or equal to 5, end game.
                 {
                     Debug.Log("You win! Game over.");
                     GameRunning = false;
-                } else
+                } else // If neither conditions are met, continue loop.
                 {
-                    if(Input.GetKeyDown(KeyCode.A))
+                    if(Input.GetKeyDown(KeyCode.A)) // If press A, attack enemy.
                     {
                         AttackEnemy();
-                    } else if(Input.GetKeyDown(KeyCode.L))
+                    } else if(Input.GetKeyDown(KeyCode.L)) // If press L, attempt to level up.
                     {
                         LevelUp();
                     }
                 }
             } else
             {
-                Debug.Log("You died. Game over.");
+                Debug.Log("You died. Game over."); 
+                GameRunning = false;
             }
         }
     }
@@ -61,17 +67,18 @@ public class GameManager : MonoBehaviour
     #region Enemy handling
     void NewEnemy()
     {
+        // Set random variables for enemy, and let player know new enemy has shown up.
         enemy_level = Random.Range(1, 5);
         enemy_healthPoints = 10.0f + (enemy_level * 5);
-        enemy_attackPoints = 2.0f * enemy_level;
+        enemy_attackPoints = 2.0f * enemy_level; 
         Debug.Log("A new level " + enemy_level + " imp appears!");
     }
 
     void AttackEnemy()
     {
-        enemy_healthPoints = enemy_healthPoints - (player_attackPoints * player_attackModifier);
+        enemy_healthPoints = enemy_healthPoints - (player_attackPoints * player_attackModifier); // Damage enemy by attack point * attack modifier.
 
-        if(enemy_healthPoints <= 0)
+        if(enemy_healthPoints <= 0) // If enemy health below or equal to 0, increase XP and create new enemy.
         {
             Debug.Log("You killed the imp!");
             IncreaseXP();
@@ -87,20 +94,22 @@ public class GameManager : MonoBehaviour
     #region Player XP/Level handling
     void IncreaseXP()
     {
+        // Increase XP by 1 or 2 * xpModifier
+
         float tempXPVal = Random.Range(1, 2) * player_xpModifier;
-        player_xp = player_xp + tempXPVal;
+        player_xp = player_xp + tempXPVal; 
         Debug.Log("You gained " + tempXPVal + " XP!");
     }
 
     void LevelUp()
     {
-        if (player_xp >= player_xpThreshold)
+        if (player_xp >= player_xpThreshold) // If player_xp is equal to or above player_xpThreshold, continue.
         {
-            player_xpThreshold = player_xpThreshold + Random.Range(2, 5);
-            player_xpModifier = player_xpModifier + 0.25f;
-            player_attackModifier = player_attackModifier + 0.25f;
-            player_healthPoints = player_healthPoints + (player_level * 5);
-            player_level = player_level + 1;
+            player_xpThreshold = player_xpThreshold + Random.Range(2, 8); // Increase XP threshold by 2-8 XP.
+            player_xpModifier = player_xpModifier + 0.25f; // Increase XP Modifier by 25%.
+            player_attackModifier = player_attackModifier + 0.25f; // Increase attack modifier by 25%.
+            player_healthPoints = player_healthPoints + (player_level * 5); // Increase health by player level * 5.
+            player_level = player_level + 1; // Increase player level.
             Debug.Log("You leveled up! You are now level" + player_level + ".");
         }
         else
